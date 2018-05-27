58091df4ae7622c19fbd337c805e2072dc1fccfa
Hai Bacti <haibacti@MacCandy.local>
2018-05-27 23:09:39 +0700

    +

--- src/MainView.js
+++ src/MainView.js
@@ -14,6 +14,7 @@ export default class MainView extends React.Component
         return (
             <WebView
                 source={{ uri: this.props.uri }}
+                scrollEnabled={false}
                 mediaPlaybackRequiresUserAction={false}
                 allowsInlineMediaPlayback={true}
             />
